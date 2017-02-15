9fd1be65c2753fe6912bd8f12bbe6daed1b69172
Daniel Carrasco <danielcarrasco17@gmail.com>
2017-02-15 08:08:49 -0300

    Refactor ReporteSeries

--- app/Stock/ReportesSeries.php
+++ app/Stock/ReportesSeries.php
@@ -34,12 +34,12 @@ trait ReportesSeries
     {
         $reporteOrigen = static::filterCampos($data, $campos);
 
+        $row_open = static::$template['row_open'];
+        $row_close = static::$template['row_close'];
+
         $reporteHead = collect($campos)->reduce(function ($carry, $elem) {
                 return $carry.'<th>'.$elem.'</th>';
         }, '');
-
-        $row_open = static::$template['row_open'];
-        $row_close = static::$template['row_close'];
 
         $reporteBody = $reporteOrigen->reduce(function ($carry, $elem) use ($row_open, $row_close, $campos) {
             return $carry
@@ -68,7 +68,7 @@ trait ReportesSeries
         $series = str_replace(' ', '', $series);
         $arrSeries = preg_grep('/[\d]+/', explode("\r\n", $series));
 
-        $arrSeries_celular = array();
+        $arrSeriesCelular = array();
 
         foreach ($arrSeries as $llave => $valor) {
             $serieTemp = $valor;
@@ -76,21 +76,22 @@ trait ReportesSeries
             if ($tipo === 'SAP') {
                 // Modificaciones de formato SAP
                 $serieTemp = preg_replace('/^01/', '1', $serieTemp);
-                $arrSeries[$llave] = (strlen($serieTemp) === '19') ? substr($serieTemp, 1, 18) : $serieTemp;
+                $arrSeries[$llave] = (strlen($serieTemp) === 19) ? substr($serieTemp, 1, 18) : $serieTemp;
             } elseif ($tipo === 'trafico') {
-                // Modificaciones de formato SCL
+                // Modificaciones de formato tabla trafico
                 $serieTemp = preg_replace('/^1/', '01', $serieTemp);
-                $arrSeries[$llave] = substr($serieTemp, 0, 14) . '0';
+                $arrSeries[$llave] = substr($serieTemp, 0, 14).'0';
             } elseif ($tipo === 'SCL') {
+                // Modificaciones de formato SCL
                 $serieTemp = preg_replace('/^1/', '01', $serieTemp);
                 $arrSeries[$llave] = $serieTemp;
             } elseif ($tipo === 'celular') {
                 $arrSeries[$llave] = (strlen($valor) === '9') ? substr($valor, 1, 8) : $valor;
-                $arrSeries_celular[$llave] = '9'.$arrSeries[$llave];
+                $arrSeriesCelular[$llave] = '9'.$arrSeries[$llave];
             }
         }
 
-        $arrSeries = ($tipo === 'celular') ? array_merge($arrSeries, $arrSeries_celular) : $arrSeries;
+        $arrSeries = ($tipo === 'celular') ? array_merge($arrSeries, $arrSeriesCelular) : $arrSeries;
 
         return collect($arrSeries);
     }
@@ -102,23 +103,60 @@ trait ReportesSeries
         return static::listToArray($series, 'SAP')
             ->map(function ($serie) {
                 // Para cada una de las series, recupera los movimientos....
-                return \DB::table(\DB::raw(config('invfija.bd_movimientos_sap').' as m'))
-                    ->select(['m.*', 'nom_usuario', 'a1.des_almacen as des_alm', 'a2.des_almacen as des_rec', 'des_cmv'])
-                    ->where('m.serie', $serie)
-                    ->leftJoin(\DB::raw(config('invfija.bd_usuarios_sap').' as u'), 'm.usuario', '=', 'u.usuario')
-                    ->leftJoin(\DB::raw(config('invfija.bd_almacenes_sap').' as a1'), function ($join) {
-                        $join->on('m.alm', '=', 'a1.cod_almacen');
-                        $join->on('m.ce', '=', 'a1.centro');
-                    })
-                    ->leftJoin(\DB::raw(config('invfija.bd_almacenes_sap').' as a2'), function ($join) {
-                        $join->on('m.rec', '=', 'a2.cod_almacen');
-                        $join->on('m.ce', '=', 'a2.centro');
-                    })
-                    ->leftJoin(\DB::raw(config('invfija.bd_cmv_sap').' as c'), 'm.cmv', '=', 'c.cmv')
-                    ->get();
+                return static::getDataReporteMovimientos($serie);
             })->reduce(function ($carry, $movimientos) use ($campos) {
                 // ...y luego junta los reportes de cada uno
                 return $carry.static::reporte($movimientos, $campos);
             }, '');
+    }
+
+    public static function getDataReporteMovimientos($serie = '')
+    {
+        return \DB::table(\DB::raw(config('invfija.bd_movimientos_sap').' as m'))
+            ->select(['m.*', 'nom_usuario', 'a1.des_almacen as des_alm', 'a2.des_almacen as des_rec', 'des_cmv'])
+            ->where('m.serie', $serie)
+            ->leftJoin(\DB::raw(config('invfija.bd_usuarios_sap').' as u'), 'm.usuario', '=', 'u.usuario')
+            ->leftJoin(\DB::raw(config('invfija.bd_almacenes_sap').' as a1'), function ($join) {
+                $join->on('m.alm', '=', 'a1.cod_almacen');
+                $join->on('m.ce', '=', 'a1.centro');
+            })
+            ->leftJoin(\DB::raw(config('invfija.bd_almacenes_sap').' as a2'), function ($join) {
+                $join->on('m.rec', '=', 'a2.cod_almacen');
+                $join->on('m.ce', '=', 'a2.centro');
+            })
+            ->leftJoin(\DB::raw(config('invfija.bd_cmv_sap').' as c'), 'm.cmv', '=', 'c.cmv')
+            ->get();
+    }
+
+    public static function reporteDespachos($series = '')
+    {
+        $campos = ['serie', 'fec_entrada_doc', 'ce', 'alm', 'des_alm', 'rec', 'des_rec', 'cmv', 'des_cmv', 'codigo_sap', 'texto_breve_material', 'lote', 'n_doc', 'referencia', 'usuario', 'nom_usuario'];
+
+        return static::listToArray($series, 'SAP')
+            ->map(function ($serie) {
+                // Para cada una de las series, recupera los movimientos....
+                return static::getDataReporteMovimientos($serie);
+            })->reduce(function ($carry, $movimientos) use ($campos) {
+                // ...y luego junta los reportes de cada uno
+                return $carry.static::reporte($movimientos, $campos);
+            }, '');
+    }
+
+    public static function getDataReporteDespachos($serie = '')
+    {
+        return \DB::table(\DB::raw(config('invfija.bd_movimientos_sap').' as m'))
+            ->select(['m.*', 'nom_usuario', 'a1.des_almacen as des_alm', 'a2.des_almacen as des_rec', 'des_cmv'])
+            ->where('m.serie', $serie)
+            ->leftJoin(\DB::raw(config('invfija.bd_usuarios_sap').' as u'), 'm.usuario', '=', 'u.usuario')
+            ->leftJoin(\DB::raw(config('invfija.bd_almacenes_sap').' as a1'), function ($join) {
+                $join->on('m.alm', '=', 'a1.cod_almacen');
+                $join->on('m.ce', '=', 'a1.centro');
+            })
+            ->leftJoin(\DB::raw(config('invfija.bd_almacenes_sap').' as a2'), function ($join) {
+                $join->on('m.rec', '=', 'a2.cod_almacen');
+                $join->on('m.ce', '=', 'a2.centro');
+            })
+            ->leftJoin(\DB::raw(config('invfija.bd_cmv_sap').' as c'), 'm.cmv', '=', 'c.cmv')
+            ->get();
     }
 }
