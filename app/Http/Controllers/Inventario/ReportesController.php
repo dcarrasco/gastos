46e75cce2505444fecfd239fdb2161616b60408b
Daniel Carrasco <danielcarrasco17@gmail.com>
2017-02-17 23:53:28 -0300

    Separa modulos reporte en inventarios

--- app/Http/Controllers/Inventario/ReportesController.php
+++ app/Http/Controllers/Inventario/ReportesController.php
@@ -3,53 +3,15 @@
 namespace App\Http\Controllers\Inventario;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Inventario\ModulosReportes;
 use Illuminate\Http\Request;
 use App\Inventario\Inventario;
 use App\Inventario\Catalogo;
 
 class ReportesController extends Controller
 {
-    protected $routeName = 'inventario.reporte';
 
-    protected $menuModulo = [];
-
-    public function __construct()
-    {
-        $this->menuModulo = [
-            'hoja' => [
-                'nombre' => trans('inventario.menu_reporte_hoja'),
-                'url'    => route($this->routeName, ['hoja']),
-                'icono'  => 'file-text-o'
-            ],
-            'material' => [
-                'nombre' => trans('inventario.menu_reporte_mat'),
-                'url'    => route($this->routeName, ['material']),
-                'icono'  => 'barcode'
-            ],
-            'materialFaltante' => [
-                'nombre' => trans('inventario.menu_reporte_faltante'),
-                'url'    => route($this->routeName, ['materialFaltante']),
-                'icono'  => 'tasks'
-            ],
-            'ubicacion' => [
-                'nombre' => trans('inventario.menu_reporte_ubicacion'),
-                'url'    => route($this->routeName, ['ubicacion']),
-                'icono'  => 'map-marker'
-            ],
-            'tiposUbicacion' => [
-                'nombre' => trans('inventario.menu_reporte_tip_ubic'),
-                'url'    => route($this->routeName, ['tiposUbicacion']),
-                'icono'  => 'th'
-            ],
-            'ajustes' => [
-                'nombre' => trans('inventario.menu_reporte_ajustes'),
-                'url'    => route($this->routeName, ['ajustes']),
-                'icono'  => 'wrench'
-            ],
-        ];
-
-        view()->share('menuModulo', $this->menuModulo);
-    }
+    use ModulosReportes;
 
     public function reporte(Request $request, $tipo = null)
     {
