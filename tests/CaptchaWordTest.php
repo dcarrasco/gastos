3cc1b5ad9867ce05fdfe983fbb39b5678b569a19
Daniel Carrasco <danielcarrasco17@gmail.com>
2018-02-02 00:26:08 -0300

    Test captchaword

---
+++ tests/CaptchaWordTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Helpers\CaptchaWord;
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CaptchaWordTest extends TestCase
+{
+    public function testWordIsString()
+    {
+        $this->assertNotEmpty(CaptchaWord::word());
+    }
+
+    public function testWordLength()
+    {
+        $this->assertEquals(8, strlen(CaptchaWord::word()));
+    }
+}
