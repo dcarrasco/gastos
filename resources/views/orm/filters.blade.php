cb01d69970c5ae32027bcccb870cfd3180f0ba83
Daniel Carrasco <danielcarrasco17@gmail.com>
2019-05-19 11:38:55 -0400

    Despliegue cantidad de filtros

--- resources/views/orm/filters.blade.php
+++ resources/views/orm/filters.blade.php
@@ -3,10 +3,7 @@
         <button id="button-filters" type="button" class="btn btn-light btn-sm dropdown-toggle m-md-2 border font-weight-bold" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <svg style="fill: #888" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"><path class="heroicon-ui" d="M2.3 7.7A1 1 0 0 1 2 7V3a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v4a1 1 0 0 1-.3.7L15 14.42V17a1 1 0 0 1-.3.7l-4 4A1 1 0 0 1 9 21v-6.59l-6.7-6.7zM4 4v2.59l6.7 6.7a1 1 0 0 1 .3.71v4.59l2-2V14a1 1 0 0 1 .3-.7L20 6.58V4H4z"/></svg>
 
-            @if ($resource->countAppliedFilters(request()) > 0)
-                {{ $resource->countAppliedFilters(request()) }}
-            @endif
-
+            {{ $resource->countAppliedFilters(request()) ?: '' }}
         </button>
 
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="button-filters" style="min-width: 20em;">
