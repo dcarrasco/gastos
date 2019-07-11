06a74a486dabe45d816ebad57fa93769796373bd
Daniel Carrasco <danielcarrasco17@gmail.com>
2019-07-10 23:15:35 -0400

    Elimina resources inventario toa stock

--- resources/views/adminbd/logs.php
+++
@@ -1,9 +0,0 @@
-<div>
-<pre>
-<?php if(count($log_file)): ?>
-<?php foreach($log_file as $linea): ?>
-<?= htmlspecialchars($linea); ?>
-<?php endforeach; ?>
-<?php endif; ?>
-</pre>
-</div> <!-- fin content-module-main -->
--- resources/views/toa/controles.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('common.app_layout')
-
-@section('modulo')
-	@include('toa.controlesForm')
-	@include('toa.controlesReporte')
-@endsection
