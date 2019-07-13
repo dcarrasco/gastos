881678ad98146b14693422e3b3ccba2ac926c604
Daniel Carrasco <danielcarrasco17@gmail.com>
2019-07-13 15:35:49 -0400

    Actualiza margen del footer

--- resources/views/common/app_footer.blade.php
+++ resources/views/common/app_footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer my-md-4">
+<footer class="footer mt-5">
     <div class="text-center text-black-40">
         <small><i class="fa fa-creative-commons"></i> 2013 &ndash; <?= date('Y'); ?></small>
     </div>
