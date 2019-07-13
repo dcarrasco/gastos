39ec80e89805b70264cfd821d87e5ffa2fe81e30
Daniel Carrasco <danielcarrasco17@gmail.com>
2019-07-13 15:36:07 -0400

    Marca menu modulo activo

--- resources/views/common/app_menu_modulo.blade.php
+++ resources/views/common/app_menu_modulo.blade.php
@@ -2,7 +2,7 @@
 <ul class="list-group list-group-flush hidden-print">
 @foreach(auth()->user()->getMenuApp() as $menuApp)
 @foreach($menuApp['modulos'] as $modulo)
-	<a href="{{ route($modulo['url']) }}" class="list-group-item list-group-item-action">
+	<a href="{{ route($modulo['url']) }}" class="list-group-item list-group-item-action {{ $modulo['selected'] ? 'active' : ''}}">
 		<span class="fa fa-{{ $modulo['icono'] }} fa-fw"></span>
 		{{ $modulo['modulo'] }}
 	</a>
