4cdde88ec1602b5238d75bbb6fc8fcd68ed6a852
Daniel Carrasco <danielcarrasco17@gmail.com>
2019-10-19 16:25:57 -0300

    Actualiza AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \Illuminate\Pagination\LengthAwarePaginator::defaultView('orm.paginator');
+        LengthAwarePaginator::defaultView('orm.paginator');
     }
 
     /**
