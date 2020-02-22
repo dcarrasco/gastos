101e2d7815018227055fb5f176c6f28ebe2cd892
Daniel Carrasco <danielcarrasco17@gmail.com>
2020-02-22 20:50:07 -0300

    Corrige despliegue valor anterior

--- app/OrmModel/Metrics/SaldoCtaCte.php
+++ app/OrmModel/Metrics/SaldoCtaCte.php
@@ -4,15 +4,43 @@ namespace App\OrmModel\Metrics;
 
 use App\Gastos\SaldoMes;
 use Illuminate\Http\Request;
+use App\OrmModel\Gastos\Gasto;
 use App\OrmModel\src\Metrics\Value;
+use Illuminate\Database\Eloquent\Builder;
 
 class SaldoCtaCte extends Value
 {
     public function calculate(Request $request): array
     {
         return $this->formattedData([
-            'currentValue' => SaldoMes::where('cuenta_id', 1)->latest()->first()->saldo_final,
-            'previousValue' => 0,
+            'currentValue' => $this->calculateSaldo($request, $this->currentRange($request)),
+            'previousValue' => $this->calculateSaldo($request, $this->previousRange($request)),
         ]);
+    }
+
+    protected function calculateSaldo(Request $request, array $range): int
+    {
+        $gastos = $this->rangedQuery($request, Gasto::class, 'fecha', $range)->get();
+        $saldo = SaldoMes::where('cuenta_id', 1)
+            ->where('anno', $gastos->max('anno'))
+            ->where('mes', $gastos->max('mes'))
+            ->first()
+            ->saldo_inicial;
+
+        return $saldo + $gastos->sum('valor_monto');
+    }
+
+
+    protected function extendFilter(Request $request, Builder $query): Builder
+    {
+        return $query->where('cuenta_id', 1);
+    }
+
+
+    public function ranges(): array
+    {
+        return [
+            'MTD' => 'Month To Date',
+        ];
     }
 }
