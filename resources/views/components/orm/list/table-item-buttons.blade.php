7ba6df1ed2e7a254c07aaf99fc9550e2735cb619
Daniel Carrasco <danielcarrasco17@gmail.com>
2020-04-05 23:32:52 -0400

    Agrega cursor pointer a icono borrar

--- resources/views/components/orm/list/table-item-buttons.blade.php
+++ resources/views/components/orm/list/table-item-buttons.blade.php
@@ -10,6 +10,7 @@
     data-url-form="{!! route($routeName.'.destroy', [$resource->getName(), $resource->model()->getKey()]) !!}"
     data-message="{!! trans('orm.delete_confirm', ['model' => $resource->getLabel(), 'item' => $resource->title() ]) !!}"
     id="delete-href"
+    style="cursor: pointer;"
 >
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M8 6V4c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2v2h5a1 1 0 0 1 0 2h-1v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V8H3a1 1 0 1 1 0-2h5zM6 8v12h12V8H6zm8-2V4h-4v2h4zm-4 4a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0v-6a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0v-6a1 1 0 0 1 1-1z"/></svg>
 </a>
