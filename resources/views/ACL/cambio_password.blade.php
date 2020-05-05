2826b6b9905408413bb85c4da5f7c1981a77c4c4
Daniel Carrasco <danielcarrasco17@gmail.com>
2020-05-04 23:16:42 -0400

    View component en login y cambio password

--- resources/views/ACL/cambio_password.blade.php
+++ resources/views/ACL/cambio_password.blade.php
@@ -1,66 +1,64 @@
-@extends('layouts.app_layout')
+<x-layout.app>
 
-@section('modulo')
+    <div class="row">
 
-<div class="row">
+        <div class="col-md-4 offset-md-4 card">
+            <div class="card-body">
+                <div class="control-group col-md-10 offset-md-1 col-xs-12 text-center">
+                    <h2>{{ trans('login.form_change_password') }}</h2>
+                </div>
 
-    <div class="col-md-4 offset-md-4 card">
-        <div class="card-body">
-            <div class="control-group col-md-10 offset-md-1 col-xs-12 text-center">
-                <h2>{{ trans('login.form_change_password') }}</h2>
-            </div>
+                <div class="control-group col-md-10 offset-md-1 col-xs-12">
+                    <hr>
+                </div>
 
-            <div class="control-group col-md-10 offset-md-1 col-xs-12">
-                <hr>
-            </div>
+                <div class="col-md-12">
+                    <x-alert :errors=$errors />
+                </div>
 
-            <div class="col-md-12">
-                <x-alert :errors=$errors />
-            </div>
+                <div class="col-md-12">
+                    {{ $msg_alerta }}
+                </div>
 
-            <div class="col-md-12">
-                {{ $msg_alerta }}
-            </div>
+                <form method="POST" id="frm_login" class="form-horizontal">
+                    @csrf
+                    <input type="hidden" name="username" value="{{ request('username') }}">
 
-            <form method="POST" id="frm_login" class="form-horizontal">
-                @csrf
-                <input type="hidden" name="username" value="{{ request('username') }}">
+                    @if ($userHasPassword)
+                        <div class="control-group col-md-10 offset-md-1 {{ $errors->has('clave_anterior') ? 'has-error' : '' }}">
+                            <label class="control-label" for="clave_anterior">{{ trans('login.input_old_password') }}</label>
+                            <input type="password" name="clave_anterior" class="form-control input-lg" maxlength="45" autocomplete="off">
+                        </div>
+                    @endif
 
-                @if ($userHasPassword)
-                    <div class="control-group col-md-10 offset-md-1 {{ $errors->has('clave_anterior') ? 'has-error' : '' }}">
-                        <label class="control-label" for="clave_anterior">{{ trans('login.input_old_password') }}</label>
-                        <input type="password" name="clave_anterior" class="form-control input-lg" maxlength="45" autocomplete="off">
+                    <div class="control-group col-md-10 offset-md-1 {{ $errors->has('nueva_clave') ? 'has-error' : '' }}">
+                        <label class="control-label" for="nueva_clave">{{ trans('login.input_new1_password') }}</label>
+                        <input type="password" name="nueva_clave" class="form-control input-lg" maxlength="45" autocomplete="off">
+                        <p class="help-block">
+                            <em><small>M&iacute;nimo 8 caracteres. Debe incluir may&uacute;sculas, min&uacute;sculas y n&uacute;meros.</small></em>
+                        </p>
                     </div>
-                @endif
 
-                <div class="control-group col-md-10 offset-md-1 {{ $errors->has('nueva_clave') ? 'has-error' : '' }}">
-                    <label class="control-label" for="nueva_clave">{{ trans('login.input_new1_password') }}</label>
-                    <input type="password" name="nueva_clave" class="form-control input-lg" maxlength="45" autocomplete="off">
-                    <p class="help-block">
-                        <em><small>M&iacute;nimo 8 caracteres. Debe incluir may&uacute;sculas, min&uacute;sculas y n&uacute;meros.</small></em>
-                    </p>
-                </div>
-
-                <div class="control-group col-md-10 offset-md-1 {{ $errors->has('nueva_clave_confirmation') ? 'has-error' : '' }}">
-                    <label class="control-label" for="nueva_clave_confirmation">{{ trans('login.input_new2_password') }}</label>
-                    <input type="password" name="nueva_clave_confirmation" class="form-control input-lg" maxlength="45" autocomplete="off">
-                </div>
-
-                <div class="control-group col-md-10 offset-md-1">
-                    <hr/>
-                </div>
-
-                <div class="control-group col-md-10 offset-md-1">
-                    <div class="controls">
-                        <button type="submit" name="btn_submit" class="col-md-12 btn btn-primary input-lg">
-                            <span class="fa fa-lock"></span> {{ trans('login.button_change_password') }}
-                        </button>
+                    <div class="control-group col-md-10 offset-md-1 {{ $errors->has('nueva_clave_confirmation') ? 'has-error' : '' }}">
+                        <label class="control-label" for="nueva_clave_confirmation">{{ trans('login.input_new2_password') }}</label>
+                        <input type="password" name="nueva_clave_confirmation" class="form-control input-lg" maxlength="45" autocomplete="off">
                     </div>
-                </div>
-            </form>
+
+                    <div class="control-group col-md-10 offset-md-1">
+                        <hr/>
+                    </div>
+
+                    <div class="control-group col-md-10 offset-md-1">
+                        <div class="controls">
+                            <button type="submit" name="btn_submit" class="col-md-12 btn btn-primary input-lg">
+                                <span class="fa fa-lock"></span> {{ trans('login.button_change_password') }}
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
+
     </div>
 
-</div>
-
-@endsection
+</x-layout.app>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,78 +1,76 @@
-@extends('layouts.app_layout')
+<x-layout.app>
 
-@section('modulo')
+    <div class="row">
 
-<div class="row">
-
-    <div class="col-md-4 offset-md-4 card">
-        <div class="card-body">
-            <div class="control-group col-md-10 offset-md-1 col-xs-12">
-                <h2 class="text-center">{{ trans('login.form_title') }}</h2>
-            </div>
-
-            <div class="control-group col-md-10 offset-md-1 col-xs-12">
-                <hr>
-            </div>
-
-            <div class="col-md-12">
-                <x-alert :errors=$errors />
-            </div>
-
-            <form method="POST" id="form_login">
-                @csrf
-                <div class="form-group col-md-10 offset-md-1 col-xs-12 {{ $errors->has('username') ? 'has-error' : '' }}">
-                    <label for="username">
-                        {{ trans('login.input_user') }}
-                    </label>
-                    <input type="text" name="username" value="{{ old('username') }}" maxlength="45" class="form-control form-control-lg" tabindex="1" autofocus="autofocus">
-                </div>
-
-                <div class="form-group col-md-10 offset-md-1 col-xs-12 {{ $errors->has('password') ? 'has-error' : '' }}">
-                    <label for="pwd">
-                        {{ trans('login.input_password') }}
-                    </label>
-                    <input type="password" name="password" maxlength="45" size="40" tabindex="2" class="form-control form-control-lg" autocomplete="off">
-                </div>
-
+        <div class="col-md-4 offset-md-4 card">
+            <div class="card-body">
                 <div class="control-group col-md-10 offset-md-1 col-xs-12">
-                    <div class="pull-right">
-                        <a href="#" id="link_cambia_password">{{ trans('login.link_change_password') }}</a>
-                    </div>
-                </div>
-
-                <div class="custom-control custom-checkbox col-md-10 offset-md-2">
-                    <input type="checkbox" name="remember" value="1" class="custom-control-input" id="remember-id">
-                    <label class="custom-control-label" for="remember-id">
-                        {{ trans('login.check_remember_me') }}
-                    </label>
+                    <h2 class="text-center">{{ trans('login.form_title') }}</h2>
                 </div>
 
                 <div class="control-group col-md-10 offset-md-1 col-xs-12">
                     <hr>
                 </div>
 
-                <div class="control-group col-md-10 offset-md-1 col-xs-12">
-                    <button type="submit" name="btn_submit" class="btn btn-success btn-lg col-md-12">
-                        {{ trans('login.button_login') }} &nbsp; <span class="fa fa-sign-in"></span>
-                    </button>
+                <div class="col-md-12">
+                    <x-alert :errors=$errors />
                 </div>
-            </form>
+
+                <form method="POST" id="form_login">
+                    @csrf
+                    <div class="form-group col-md-10 offset-md-1 col-xs-12 {{ $errors->has('username') ? 'has-error' : '' }}">
+                        <label for="username">
+                            {{ trans('login.input_user') }}
+                        </label>
+                        <input type="text" name="username" value="{{ old('username') }}" maxlength="45" class="form-control form-control-lg" tabindex="1" autofocus="autofocus">
+                    </div>
+
+                    <div class="form-group col-md-10 offset-md-1 col-xs-12 {{ $errors->has('password') ? 'has-error' : '' }}">
+                        <label for="pwd">
+                            {{ trans('login.input_password') }}
+                        </label>
+                        <input type="password" name="password" maxlength="45" size="40" tabindex="2" class="form-control form-control-lg" autocomplete="off">
+                    </div>
+
+                    <div class="control-group col-md-10 offset-md-1 col-xs-12">
+                        <div class="pull-right">
+                            <a href="#" id="link_cambia_password">{{ trans('login.link_change_password') }}</a>
+                        </div>
+                    </div>
+
+                    <div class="custom-control custom-checkbox col-md-10 offset-md-2">
+                        <input type="checkbox" name="remember" value="1" class="custom-control-input" id="remember-id">
+                        <label class="custom-control-label" for="remember-id">
+                            {{ trans('login.check_remember_me') }}
+                        </label>
+                    </div>
+
+                    <div class="control-group col-md-10 offset-md-1 col-xs-12">
+                        <hr>
+                    </div>
+
+                    <div class="control-group col-md-10 offset-md-1 col-xs-12">
+                        <button type="submit" name="btn_submit" class="btn btn-success btn-lg col-md-12">
+                            {{ trans('login.button_login') }} &nbsp; <span class="fa fa-sign-in"></span>
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
+
+        <script type="text/javascript">
+            $( document ).ready(function() {
+                $('#link_cambia_password').click(function(e) {
+                    e.preventDefault();
+                    username = $('#form_login input[name="username"]').val();
+                    if (username !== '') {
+                        window.location.href = '{{ route("acl.cambiaPassword", [""]) }}/'+username;
+                    } else {
+                        alert('Debe ingresa un nombre de usuario para cambiarle la clave');
+                    }
+                });
+            });
+        </script>
     </div>
 
-    <script type="text/javascript">
-        $( document ).ready(function() {
-            $('#link_cambia_password').click(function(e) {
-                e.preventDefault();
-                username = $('#form_login input[name="username"]').val();
-                if (username !== '') {
-                    window.location.href = '{{ route("acl.cambiaPassword", [""]) }}/'+username;
-                } else {
-                    alert('Debe ingresa un nombre de usuario para cambiarle la clave');
-                }
-            });
-        });
-    </script>
-</div>
-
-@endsection
+</x-layout.app>
