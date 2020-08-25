ebafda786229578803d402b5572979e1d6e6ca93
Daniel Carrasco <danielcarrasco17@gmail.com>
2020-08-24 22:51:13 -0400

    Nuevos tests

---
+++ tests/Feature/OrmModel/Metrics/PartitionTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Tests\Unit\OrmModel\Metrics;
+
+use Tests\TestCase;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use App\OrmModel\src\Metrics\Partition;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PartitionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $partition;
+    protected $request;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->request = $this->makeMock(Request::class, []);
+
+        $this->partition = new class() extends Partition {
+            public function calculate(Request $request): Collection
+            {
+                return collect([1 => 100, 2 =>50, 3=>20]);
+            }
+        };
+    }
+
+    protected function makeMock(string $class, array $methods)
+    {
+        return $this->getMockBuilder($class)
+            ->disableOriginalConstructor()
+            ->setMethods($methods)
+            ->getMock();
+    }
+
+    public function testCalculate()
+    {
+        $this->assertCount(3, $this->partition->calculate($this->request));
+
+        $partition = new class() extends Partition {
+        };
+        $this->assertEmpty($partition->calculate($this->request));
+    }
+
+    public function testAggregators()
+    {
+        $usuarios = factory(\App\Acl\Usuario::class, 5)->create(['activo' => 1]);
+        $usuarios = factory(\App\Acl\Usuario::class, 2)->create(['activo' => 0]);
+
+        $this->assertEquals(['1' => '5', '0' => '2'], $this->partition->count($this->request, \App\OrmModel\Acl\Usuario::class, 'activo')->all());
+
+        $this->assertEquals(['1' => '5', '0' => '0'], $this->partition->sum($this->request, \App\OrmModel\Acl\Usuario::class, 'activo', 'activo')->all());
+    }
+
+    public function testAggregatorsWithRelations()
+    {
+        $app1 = factory(\App\Acl\App::class)->create();
+        $app2 = factory(\App\Acl\App::class)->create();
+
+        $modulo1 = factory(\App\Acl\Modulo::class, 5)->create(['app_id' => $app1->id]);
+        $modulo2 = factory(\App\Acl\Modulo::class, 2)->create(['app_id' => $app2->id]);
+
+        $this->assertEquals(
+            [$app1->descripcion => '5', $app2->descripcion => '2'],
+            $this->partition->count($this->request, \App\OrmModel\Acl\Modulo::class, 'acl_app.descripcion', 'app')->all()
+        );
+
+        $this->assertEquals(
+            [$app1->descripcion => $modulo1->sum('orden'), $app2->descripcion => $modulo2->sum('orden')],
+            $this->partition->sum($this->request, \App\OrmModel\Acl\Modulo::class, 'acl_app.descripcion', 'acl_modulo.orden', 'app')->all()
+        );
+    }
+
+    public function testContent()
+    {
+        $this->assertIsArray($this->partition->content($this->request)->toHtml()->getData());
+        $this->assertArrayHasKey('cardId', $this->partition->content($this->request)->toHtml()->getData());
+        $this->assertArrayHasKey('script', $this->partition->content($this->request)->toHtml()->getData());
+    }
+
+    public function testContentScript()
+    {
+        $this->assertIsString($this->partition->contentScript($this->request)->toHtml());
+    }
+}
---
+++ tests/Feature/OrmModel/Metrics/TrendTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Unit\OrmModel\Metrics;
+
+use Tests\TestCase;
+use Illuminate\Http\Request;
+use App\OrmModel\Acl\App;
+use App\OrmModel\src\Resource;
+use Illuminate\Support\Collection;
+use Illuminate\Support\HtmlString;
+use App\Acl\App as AppModel;
+use App\OrmModel\src\Metrics\Trend;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TrendTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $trend;
+    protected $request;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->request = $this->makeMock(Request::class, []);
+
+        $this->trend = new class() extends Trend {
+        };
+    }
+
+    protected function makeMock(string $class, array $methods)
+    {
+        return $this->getMockBuilder($class)
+            ->disableOriginalConstructor()
+            ->setMethods($methods)
+            ->getMock();
+    }
+
+    public function testCalculate()
+    {
+        $this->assertEmpty($this->trend->calculate($this->request));
+    }
+
+    public function testAggregators()
+    {
+        $app1 = factory(AppModel::class)->create(['orden' => 10]);
+        $app2 = factory(AppModel::class)->create(['orden' => 20]);
+        $app3 = factory(AppModel::class)->create(['orden' => 30]);
+        $app4 = factory(AppModel::class)->create(['orden' => 40]);
+
+        $request = $this->makeMock(Request::class, ['input']);
+        $request->expects($this->any())->method('input')->willReturn('MTD');
+
+        $this->assertEquals(100, $this->trend->sumByDays($request, App::class, 'orden')->last());
+        $this->assertEquals(100, $this->trend->sumByWeeks($request, App::class, 'orden')->last());
+        $this->assertEquals(100, $this->trend->sumByMonths($request, App::class, 'orden')->last());
+        $this->assertEquals(100, $this->trend->sumByYears($request, App::class, 'orden')->last());
+
+        $this->assertEquals(4, $this->trend->countByDays($request, App::class)->last());
+        $this->assertEquals(4, $this->trend->countByWeeks($request, App::class)->last());
+        $this->assertEquals(4, $this->trend->countByMonths($request, App::class)->last());
+        $this->assertEquals(4, $this->trend->countByYears($request, App::class)->last());
+    }
+
+    public function testContentScript()
+    {
+        $this->assertIsString($this->trend->contentScript($this->request)->toHtml());
+    }
+}
