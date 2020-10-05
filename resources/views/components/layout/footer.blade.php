1497d9cff6ccf90b4ff72dd723e448b98f4cf89b
Daniel Carrasco <danielcarrasco17@gmail.com>
2020-10-04 21:11:23 -0300

    Mejora presentacion del footer

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="footer mt-5">
-    <div class="text-center text-black-40">
+<footer class="footer mt-6">
+    <div class="text-center text-gray-500">
         <small><i class="fa fa-creative-commons"></i> 2013 &ndash; <?= date('Y'); ?></small>
     </div>
 </footer>
