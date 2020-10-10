ed58c7ce67e6996b647e4fd51e482a84bc55dd24
Daniel Carrasco <danielcarrasco17@gmail.com>
2020-10-10 16:44:54 -0300

    Agrega text-gray-600 para form items

--- resources/views/components/orm/item-form.blade.php
+++ resources/views/components/orm/item-form.blade.php
@@ -1,5 +1,5 @@
-<div class="grid grid-cols-4 px-5 py-5">
-    <div class="col-span-1 {{ $errors->has($field->getModelAttribute($resource)) ? 'text-red-700' : 'text-gray-600' }}">
+<div class="grid grid-cols-4 px-5 py-5 text-gray-600">
+    <div class="col-span-1 {{ $errors->has($field->getModelAttribute($resource)) ? 'text-red-700' : '' }}">
         {{ $field->getName() }}
 
         @if ($field->isRequired())
