5536a37fd7915ef0b1739655a69a62f9e432f3b6
Daniel Carrasco <danielcarrasco17@gmail.com>
2020-10-10 10:13:59 -0300

    Update footer.blade.php

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,5 +1,3 @@
-<footer class="footer mt-6">
-    <div class="text-center text-gray-500">
-        <small><i class="fa fa-creative-commons"></i> 2013 &ndash; <?= date('Y'); ?></small>
-    </div>
+<footer class="footer pt-6 text-center text-xs text-gray-500">
+    <i class="fa fa-creative-commons"></i> 2013 &ndash; <?= date('Y'); ?>
 </footer>
