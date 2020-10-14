9487af581e5f455fc9ef98d5073a63910e3e3e18
Daniel Carrasco <danielcarrasco17@gmail.com>
2020-10-14 17:33:32 -0300

    Actualiza seeder de rol-modulo con abilities

--- database/seeders/TablaACLRolModuloSeeder.php
+++ database/seeders/TablaACLRolModuloSeeder.php
@@ -17,12 +17,53 @@ class TablaACLRolModuloSeeder extends Seeder
         // ---------------------------------------------------------------------
         // GASTOS
         // ---------------------------------------------------------------------
-        DB::table('acl_rol_modulo')->insert(['rol_id' => 1, 'modulo_id' => 1, 'created_at' => now()]);
-        DB::table('acl_rol_modulo')->insert(['rol_id' => 1, 'modulo_id' => 2, 'created_at' => now()]);
-        DB::table('acl_rol_modulo')->insert(['rol_id' => 1, 'modulo_id' => 3, 'created_at' => now()]);
-        DB::table('acl_rol_modulo')->insert(['rol_id' => 1, 'modulo_id' => 4, 'created_at' => now()]);
-        DB::table('acl_rol_modulo')->insert(['rol_id' => 1, 'modulo_id' => 5, 'created_at' => now()]);
-        DB::table('acl_rol_modulo')->insert(['rol_id' => 1, 'modulo_id' => 6, 'created_at' => now()]);
-        DB::table('acl_rol_modulo')->insert(['rol_id' => 1, 'modulo_id' => 7, 'created_at' => now()]);
+        DB::table('acl_rol_modulo')->insert([
+            'rol_id' => 1,
+            'modulo_id' => 1,
+            'abilities' => '["view", "view-any", "create", "update", "delete"]',
+            'created_at' => now(),
+        ]);
+
+        DB::table('acl_rol_modulo')->insert([
+            'rol_id' => 1,
+            'modulo_id' => 2,
+            'abilities' => '["view-any", "create", "delete"]',
+            'created_at' => now(),
+        ]);
+
+        DB::table('acl_rol_modulo')->insert([
+            'rol_id' => 1,
+            'modulo_id' => 3,
+            'abilities' => '["view-any", "create", "delete"]',
+            'created_at' => now(),
+        ]);
+
+        DB::table('acl_rol_modulo')->insert([
+            'rol_id' => 1,
+            'modulo_id' => 4,
+            'abilities' => '["create", "delete"]',
+            'created_at' => now(),
+        ]);
+
+        DB::table('acl_rol_modulo')->insert([
+            'rol_id' => 1,
+            'modulo_id' => 5,
+            'abilities' => '["view-any", "create", "delete"]',
+            'created_at' => now(),
+        ]);
+
+        DB::table('acl_rol_modulo')->insert([
+            'rol_id' => 1,
+            'modulo_id' => 6,
+            'abilities' => '["view", "view-any", "create", "update", "delete"]',
+            'created_at' => now(),
+        ]);
+
+        DB::table('acl_rol_modulo')->insert([
+            'rol_id' => 1,
+            'modulo_id' => 7,
+            'abilities' => '["view-any", "create", "delete"]',
+            'created_at' => now(),
+        ]);
     }
 }
