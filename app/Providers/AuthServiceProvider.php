e96001a170d9a3301a1e61f46dc9818b79aa611f
Daniel Carrasco <danielcarrasco17@gmail.com>
2020-11-07 10:37:52 -0300

    Actualiza clases en AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,20 +2,8 @@
 
 namespace App\Providers;
 
-use App\Models\Acl\App;
-use App\Models\Acl\Rol;
-use App\Models\Acl\Modulo;
-use App\Models\Acl\Usuario;
 use App\Policies\AclPolicy;
-use App\Models\Gastos\Banco;
-use App\Models\Gastos\Gasto;
-use App\Models\Gastos\Cuenta;
-use App\Models\Gastos\SaldoMes;
-use App\Models\Gastos\TipoGasto;
-use App\Models\Gastos\TipoCuenta;
 use Illuminate\Support\Facades\Gate;
-use App\Models\Gastos\GlosaTipoGasto;
-use App\Models\Gastos\TipoMovimiento;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -29,20 +17,20 @@ class AuthServiceProvider extends ServiceProvider
         'App\Model' => 'App\Policies\ModelPolicy',
 
         // ORM Acl classes
-        Modulo::class => AclPolicy::class,
-        Rol::class => AclPolicy::class,
-        App::class => AclPolicy::class,
-        Usuario::class => AclPolicy::class,
+        \App\Models\Acl\Modulo::class => AclPolicy::class,
+        \App\Models\Acl\Rol::class => AclPolicy::class,
+        \App\Models\Acl\App::class => AclPolicy::class,
+        \App\Models\Acl\Usuario::class => AclPolicy::class,
 
         // ORM Gastos classes
-        Banco::class => AclPolicy::class,
-        TipoCuenta::class => AclPolicy::class,
-        Cuenta::class => AclPolicy::class,
-        TipoMovimiento::class => AclPolicy::class,
-        TipoGasto::class => AclPolicy::class,
-        GlosaTipoGasto::class => AclPolicy::class,
-        SaldoMes::class => AclPolicy::class,
-        Gasto::class => AclPolicy::class,
+        \App\Models\Gastos\Banco::class => AclPolicy::class,
+        \App\Models\Gastos\TipoCuenta::class => AclPolicy::class,
+        \App\Models\Gastos\Cuenta::class => AclPolicy::class,
+        \App\Models\Gastos\TipoMovimiento::class => AclPolicy::class,
+        \App\Models\Gastos\TipoGasto::class => AclPolicy::class,
+        \App\Models\Gastos\GlosaTipoGasto::class => AclPolicy::class,
+        \App\Models\Gastos\SaldoMes::class => AclPolicy::class,
+        \App\Models\Gastos\Gasto::class => AclPolicy::class,
     ];
 
     /**
