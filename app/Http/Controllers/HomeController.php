5462a65768934a92abaddfb193346c506599e345
Daniel Carrasco <danielcarrasco17@gmail.com>
2020-11-07 19:11:32 -0300

    Elimina middleware auth de clase HomeController (esta en web.php)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,16 +19,6 @@ class HomeController extends Controller
     use OrmCard;
 
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
