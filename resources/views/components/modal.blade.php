bf9328571e992cce890b5bff96ff9286bf0f4d6a
Daniel Carrasco <danielcarrasco17@gmail.com>
2021-02-27 19:39:00 -0300

    Refactor modal delete

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,29 @@
+<div x-show="openDeleteModal" style="display: none;">
+    <div class="fixed z-10 inset-0 overflow-y-auto">
+        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 text-base">
+            <div class="fixed inset-0 transition-opacity" x-on:click="openDeleteModal=false">
+                <div class="absolute inset-0 bg-gray-800 opacity-50"></div>
+            </div>
+
+            <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>&#8203;
+
+            <div x-show.transition.opacity.duration.300ms="openDeleteModal" class="p-4 inline-block align-bottom bg-white rounded-lg border text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline" style="display: none;">
+
+                <div class="flex justify-between pt-2 pb-5">
+                    {{ $title }}
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close" x-on:click="openDeleteModal=false">
+                        <span class="fa fa-close"></span>
+                    </button>
+                </div>
+
+                <div class="py-2">
+                    {{ $slot }}
+                </div>
+
+                <div class="flex justify-end py-2">
+                    {{ $buttons }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/components/orm/list/modal-delete.blade.php
+++ resources/views/components/orm/list/modal-delete.blade.php
@@ -1,44 +1,27 @@
 @props([
     'resource' => null,
-    'routeName' => '',
 ])
-<div x-show="openDeleteModal" style="display: none;">
-    <div class="fixed z-10 inset-0 overflow-y-auto">
-        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 text-base">
-            <div class="fixed inset-0 transition-opacity" @click="openDeleteModal=false">
-                <div class="absolute inset-0 bg-gray-800 opacity-75"></div>
-            </div>
 
-            <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>&#8203;
+<x-modal>
+    <x-slot name="title">
+        <h5 class="text-xl font-bold">
+            Borrar {{ optional($resource)->getLabel() }}
+        </h5>
+    </x-slot>
 
-            <div class="p-4 inline-block align-bottom bg-white rounded-lg border text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+    <x-slot name="buttons">
+        <x-button color="secondary" class="mx-2" x-on:click="openDeleteModal=false">
+            {{ trans('orm.button_cancel') }}
+        </x-button>
+        <form method="POST" x-bind:action="urlDelete" action="">
+            @csrf
+            @method('DELETE')
+            <x-button type="submit" color="danger">
+                <span class="fa fa-trash-o"></span>
+                {{ trans('orm.button_delete') }}
+            </x-button>
+        </form>
+    </x-slot>
 
-                <div class="flex justify-between py-2">
-                    <h5 class="text-xl font-bold" id="exampleModalCenterTitle">
-                        Borrar {{ optional($resource)->getLabel() }}
-                    </h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="openDeleteModal=false">
-                        <span class="fa fa-close"></span>
-                    </button>
-                </div>
-
-                <div x-html="deleteMessage" class="py-2"></div>
-
-                <div class="flex justify-end py-2">
-                    <x-button color="secondary" @click="openDeleteModal=false" class="mx-2">
-                        {{ trans('orm.button_cancel') }}
-                    </x-button>
-                    <form method="POST" x-bind:action="urlDelete" action="" id="formDelete">
-                        @csrf
-                        @method('DELETE')
-                        <x-button type="submit" color="danger">
-                            <span class="fa fa-trash-o"></span>
-                            {{ trans('orm.button_delete') }}
-                        </x-button>
-                    </form>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</div>
+    <div x-html="deleteMessage"></div>
+</x-modal>
