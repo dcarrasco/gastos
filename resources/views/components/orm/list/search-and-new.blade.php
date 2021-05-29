66e4db96e30e19aa0594e313bda275411da02c5b
Daniel Carrasco <danielcarrasco17@gmail.com>
2021-05-29 18:48:21 -0400

    Agrega color a search input

--- resources/views/components/orm/list/search-and-new.blade.php
+++ resources/views/components/orm/list/search-and-new.blade.php
@@ -1,11 +1,22 @@
 @props(['resource'])
+@php
+    $activeSearchClass = ! empty(Request::input($resource->urlSearchKey())) ? 'bg-yellow-100' : '';
+@endphp
 
 <form method="GET" class="flex justify-between py-2">
     <div id="{{ $resource->urlSearchKey() }}_group" class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center">
+        <div class="absolute inset-y-0 left-0 flex items-center text-gray-500">
             <x-heroicon.search />
         </div>
-        <input type="text" name="{{ $resource->urlSearchKey() }}" value="{{ Request::input($resource->urlSearchKey()) }}" class="pl-10 py-2 rounded-md outline-none focus:shadow-outline" id="{{ $resource->urlSearchKey() }}" maxlength="30" placeholder="{{ trans('orm.filter') }}">
+        <input
+            type="text"
+            name="{{ $resource->urlSearchKey() }}"
+            value="{{ Request::input($resource->urlSearchKey()) }}"
+            class="{{ $activeSearchClass }} pl-10 py-2 rounded-md outline-none focus:shadow-outline"
+            id="{{ $resource->urlSearchKey() }}"
+            maxlength="30"
+            placeholder="{{ trans('orm.filter') }}"
+        >
     </div>
 
     @can('create', $resource->model())
