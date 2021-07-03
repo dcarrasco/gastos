06979004d0d6622aaef344184031713187d2a074
Daniel Carrasco <danielcarrasco17@gmail.com>
2021-07-03 11:49:09 -0400

    Refactpor visa pdf parser

--- app/Models/Gastos/ParserMasivo/VisaPdfParser.php
+++ app/Models/Gastos/ParserMasivo/VisaPdfParser.php
@@ -11,9 +11,16 @@ use App\Models\Gastos\GlosaTipoGasto;
 
 class VisaPdfParser extends GastosParser
 {
-    protected $descripcion = 'PDF';
+    protected $descripcion = 'PDF Cartola';
 
     protected $cuentaAsociada = 2;
+
+    protected $campos = [
+        'fecha' => [2, 2],
+        'serie' => [3, 4],
+        'descripcion' => [5, -5],
+        'monto' => [-1, -1],
+    ];
 
     public function procesaMasivo(Request $request): Collection
     {
@@ -42,15 +49,9 @@ class VisaPdfParser extends GastosParser
     protected function filtrarLineasValidas(Request $request): VisaPdfParser
     {
         $this->datosMasivos = $this->datosMasivos
-            ->filter(function ($linea) {
-                return preg_match('/\t/', $linea) == 0;
-            })
-            ->filter(function ($linea) {
-                return collect(explode(" ", $linea))->count() >= 11;
-            })
-            ->filter(function ($linea) {
-                return preg_match('/[0-9]{4}/', $linea) === 1;
-            });
+            ->filter(fn($linea) => preg_match('/\t/', $linea) == 0)
+            ->filter(fn($linea) => collect(explode(" ", $linea))->count() >= 11)
+            ->filter(fn($linea) => preg_match('/[0-9]{4}/', $linea) === 1);
 
         return $this;
     }
@@ -58,9 +59,7 @@ class VisaPdfParser extends GastosParser
     protected function procesaLineas(Request $request): VisaPdfParser
     {
         $this->datosMasivos = $this->datosMasivos
-            ->map(function ($linea) use ($request) {
-                return $this->procesaLineaMasivo($request, $linea);
-            });
+            ->map(fn($linea) => $this->procesaLineaMasivo($request, $linea));
 
         return $this;
     }
@@ -89,75 +88,63 @@ class VisaPdfParser extends GastosParser
         $camposFiltro = ['cuenta_id', 'anno', 'fecha', 'serie', 'monto'];
 
         $this->datosMasivos = $this->datosMasivos
-            ->filter(function ($gasto) use ($request, $camposFiltro) {
-                return Gasto::where($gasto->only($camposFiltro))->get()->count() == 0;
-            });
+            ->filter(fn($gasto) => Gasto::where($gasto->only($camposFiltro))->get()->count() == 0);
 
         return $this;
     }
 
     protected function getTipoGasto(Request $request, Collection $linea): TipoGasto
     {
-        $glosa = $this->getGlosa($linea);
+        $glosa = strtoupper($this->getGlosa($linea));
 
         $glosaTipoGasto = $this->glosasTipoGasto
-            ->first(function ($glosaTipoGasto) use ($glosa) {
-                return strpos(strtoupper($glosa), strtoupper($glosaTipoGasto->glosa)) !== false;
-            })
+            ->first(fn($glosaTipoGasto) => strpos($glosa, strtoupper($glosaTipoGasto->glosa)) !== false)
             ?? new GlosaTipoGasto();
 
         return $glosaTipoGasto->tipoGasto ?? new TipoGasto();
     }
 
-    protected function getIndexFecha(Collection $linea): int
-    {
-        return 1;
-    }
-
     protected function getFecha(Collection $linea): Carbon
     {
-        $fecha = explode('/', $linea->get($this->getIndexFecha($linea)));
+        $fecha = explode('/', $this->getCampo('fecha', $linea));
 
         return (new Carbon())->create(2000 + (int)$fecha[2], $fecha[1], $fecha[0], 0, 0, 0);
     }
 
-    protected function getIndexSerie(Collection $linea)
-    {
-        $indexFecha = $this->getIndexFecha($linea);
-        $serie = $linea->get($indexFecha + 1) . $linea->get($indexFecha + 2);
-
-        if (preg_match('/^[0-9]{12}$/', $serie) === 1) {
-            return range($indexFecha + 1, $indexFecha + 2);
-        }
-
-        return range($indexFecha + 1, $indexFecha + 1);
-    }
-
     protected function getSerie(Collection $linea): string
     {
-        return $linea->only($this->getIndexSerie($linea))->implode('');
+        return str_replace(' ', '', $this->getCampo('serie', $linea));
     }
 
-    protected function getGlosa($linea = []): string
+    protected function getGlosa(Collection $linea): string
     {
-        $glosa = '';
-        $indexMonto = 3;
-
-        while ($linea[$indexMonto] != '$') {
-            $glosa .= $linea[$indexMonto] . ' ';
-            $indexMonto++;
-        }
-
-        return trim($glosa);
+        return $this->getCampo('descripcion', $linea);
     }
 
     protected function getMonto(Collection $linea): int
     {
-        return (int) str_replace('.', '', str_replace('$', '', $linea->last()));
+        $monto = $this->getCampo('monto', $linea);
+
+        return (int) str_replace('.', '', str_replace('$', '', $monto));
     }
 
-    protected function montosConSigno(Collection $linea): bool
+    protected function getCampo(string $campo, Collection $linea): string
     {
-        return strpos($linea->last(), '$') !== false;
+        return $this->getRangeCamposLinea($linea, collect($this->campos)->get($campo, []))
+            ->map(fn($campo) => trim($linea->get($campo, '')))
+            ->implode(' ');
+
+    }
+
+    protected function getRangeCamposLinea(Collection $linea, array $limites): Collection
+    {
+        return collect(
+            range($this->getCampoLinea($linea, $limites[0]), $this->getCampoLinea($linea, $limites[1]))
+        );
+    }
+
+    protected function getCampoLinea(Collection $linea, int $posicion): string
+    {
+        return ($posicion < 0) ? $linea->count() + $posicion : $posicion - 1;
     }
 }
