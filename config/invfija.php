2a34ccfff1075293bcaf8085b23b9108d4675316
Daniel Carrasco <danielcarrasco17@gmail.com>
2022-02-14 23:01:47 -0300

    Agrega nuevas rutas para chequeo

--- config/invfija.php
+++ config/invfija.php
@@ -5,11 +5,13 @@ return [
 
     // Rutas ORM que necesitan correccion para mostrar bien menu selected
     'gastosConfig_create' => 'gastosConfig.index',
+    'gastosConfig_store' => 'gastosConfig.index',
     'gastosConfig_show' => 'gastosConfig.index',
     'gastosConfig_edit' => 'gastosConfig.index',
     'gastosConfig_update' => 'gastosConfig.index',
 
     'aclConfig_create' => 'aclConfig.index',
+    'aclConfig_store' => 'aclConfig.index',
     'aclConfig_show' => 'aclConfig.index',
     'aclConfig_edit' => 'aclConfig.index',
     'aclConfig_update' => 'aclConfig.index',
