0d706c58e24df794b7a6c0520855c3942953b095
Daniel Carrasco <danielcarrasco17@gmail.com>
2024-03-03 10:42:21 -0300

    Laravel 11

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,14 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-class Controller extends BaseController
+abstract class Controller
 {
-    use AuthorizesRequests;
-    use DispatchesJobs;
-    use ValidatesRequests;
+    //
 }
